09a486aafb68ec53abcdac9a7727be46b27dbae8
nicoschiavon <nico.schiavon@gmail.com>
2024-12-10 11:36:19 -0300

    implementacion front

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html', {})
+
+def acerca(request):
+    return render(request, 'Acerca-de.html')
